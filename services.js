317332c5913d074108f745c60f2ddf3922b89e7f
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-05-01 23:39:18 +0100

    made changes

--- services.js
+++ services.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 export const originUrl =
   process.env.NODE_ENV === "production"
-    ? "https://diasporaimpactgroup.web.app"
+    ? "https://durablewelfaremarshal.web.app"
     : "http://localhost:3000";
